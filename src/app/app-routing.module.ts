6784e9861c91b141530a2af2ad7ca6cf559804aa
frexdel <frexdel@gmail.com>
2020-01-15 12:59:55 +0100

    Add NotFoundComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {CinemaComponent} from './cinema/cinema.component';
+import {NotfoundComponent} from './notfound/notfound.component';
+// import {VilleComponent} from './ville/ville.component';
 
 const routes: Routes = [
+  {path:"",component:NotfoundComponent},
   {path:"cinema",component:CinemaComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CinemaComponent } from './cinema/cinema.component';
+import {HttpClientModule} from '@angular/common/http';
+import { NotfoundComponent } from './notfound/notfound.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CinemaComponent
+    CinemaComponent,
+    NotfoundComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
